eb3f3373d47dd9c587d41e9e93a5708e9311481b
MaHeR <maher.a@hc-tech.jp>
2019-05-09 15:07:36 +0900

    Scroll function v2

--- js/index.js
+++ js/index.js
@@ -34,3 +34,21 @@ var scrollFx = function() {
   }
 };
 //Scroll function
+var scrolly = function(e) {
+  e.preventDefault();
+  var target = this.hash;
+  var $target = $(target);
+
+  $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+  }, 300, 'swing', function () {
+      window.location.hash = target;
+  });
+}
+var init = function() {
+  burger.addEventListener('click', openMenu, false);
+  window.addEventListener('scroll', scrollFx, false);
+  window.addEventListener('load', scrollFx, false);
+  $('a[href^="#"]').on('click',scrolly);
+};
+doc.on('ready', init);
