7d349d121264220e4c781c56abe25cb5631557a3
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2019-12-16 23:42:12 +0900

    Adding new variables

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
-
+var menu = document.querySelector('.nav__list');
+var burger = document.querySelector('.burger');
+var doc = $(document);
+var l = $('.scrolly');
+var panel = $('.panel');
+var vh = $(window).height();
+var openMenu = function() {
+  burger.classList.toggle('burger--active');
+  menu.classList.toggle('nav__list--active');
 };
 $('.card').click(function(){
   $(this).toggleClass('flipped');
