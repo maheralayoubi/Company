0b5bca06f88efbea33c1c46bafe02d913089b391
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2020-03-12 13:47:28 +0900

    Fix the target

--- ara/index.php
+++ ara/index.php
@@ -243,8 +243,8 @@ if($page_flag == 1) {
             <div class="panel__content_three">
               <div class="third-sec-images">
                 <p class="first-sec-text-two works-title-sp">أعمال&nbsp;&nbsp;<span class="news-line works-line"></span></p>
-                <a href="https://youtubeboost.jp/">
-                  <img target="_blank" class="third-sec-img" src="../works-1.png" alt="">
+                <a target="_blank" href="https://youtubeboost.jp/">
+                  <img class="third-sec-img" src="../works-1.png" alt="">
                 </a>
                 <img class="third-sec-img" src="../works-2.png" alt="">
                 <img class="third-sec-img" src="../works-3.png" alt="">
