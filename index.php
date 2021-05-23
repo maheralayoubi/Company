49f58f00222bdb66436d985aaf816b82f97cee16
Maher <maher91syr@gmail.com>
2021-05-23 15:45:12 +0900

    YOutube boost removed

--- index.php
+++ index.php
@@ -242,7 +242,6 @@ if($page_flag == 1) {
             <div class="panel__content_three">
               <div class="third-sec-images">
                 <p class="first-sec-text-two works-title-sp">WORKS&nbsp;&nbsp;<span style="width:330px;" class="news-line works-line"></span></p>
-                <img class="third-sec-img" src="works-1.png" alt="">
                 <img class="third-sec-img" src="works-2.png" alt="">
                 <img class="third-sec-img" src="works-3.png" alt="">
                 <img class="third-sec-img" src="works-4.png" alt="">
